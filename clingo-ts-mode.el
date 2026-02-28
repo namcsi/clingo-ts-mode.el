bc79ac43385f3e7905a6c7219d5adf8fc0a96bd2
Amade Nemes <nemesamade@gmail.com>
2026-02-28 18:58:17 +0100

    Init project with eldev.

---
+++ clingo-ts-mode.el
@@ -0,0 +1,39 @@
+;;; clingo-ts-mode.el --- A tree-sitter powered major mode for clingo -*- lexical-binding:t -*-
+
+;; Copyright (C) 2026 Amadé Nemes
+
+;; Author: Amadé Nemes <nemesamade@gmail.com>
+;; Maintainer: Amadé Nemes <nemesamade@gmail.com>
+;; Created: Feb 2026
+;; URL: https://github.com/namcsi/clingo-ts-mode.el
+;; Version: 0.1.0
+;; Keywords: languages
+;; Package-Requires: ((emacs "30.2"))
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;;; This package defines a tree-sitter powered major mode for the
+;;; input language of the Potassco Answer Set Programming (ASP) system
+;;; clingo.
+
+;;; Code:
+
+(provide 'clingo-ts-mode)
+
+;;; clingo-ts-mode.el ends here
